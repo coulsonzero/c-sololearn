326344113672bf769583e5781f95679d7cb6fb50
coulsonzero <coulson2021@163.com>
2023-06-21 14:32:12 +0800

    commit_message: update

--- main.c
+++ main.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
+#include <time.h>
+
+void calc_time();
 
 int main() {
-    printf("Hello, World!\n");
+    calc_time();
     return 0;
 }
 // Output: Hello, World!
+
+void calc_time() {
+
+    clock_t start, finish;
+    double duration;
+    /* 测量一个事件持续的时间*/
+    printf( "Time to do empty loops is ");
+    start = clock();
+    for (long i = 0 ; i < 1000000000L; ++i) {
+
+    }
+    finish = clock();
+    duration = (double)(finish - start) / CLOCKS_PER_SEC;
+    printf( "%f seconds\n", duration);
+//    system("pause");
+}
---
+++ makefile
@@ -0,0 +1,6 @@
+# make push
+push:
+	@git add .
+	@git commit -m "commit_message: update"
+	@git status
+	@git push origin master
